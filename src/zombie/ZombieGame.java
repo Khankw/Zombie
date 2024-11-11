ad20c4d7784582679fb7ddbbb1467f6146efbb98
Khankw <jskw247@gmail.com>
2024-11-12 05:46:38 +0900

    Feat ZombieGame upgrade() attack()

--- src/zombie/ZombieGame.java
+++ src/zombie/ZombieGame.java
@@ -5,8 +5,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ZombieGame {
+	private final int HP = 1;
+	private final int POW = 2;
+	private final int ACC = 3;
 	private final int MOVE = 1;
 	private final int EXIT = 0;
+	private final int ATTACK = 1;
+	private final int POTION = 2;
+
+	private final int MAP_SIZE = 40;
 
 	private Scanner scan = new Scanner(System.in);
 	private RandomManager ran = RandomManager.getInstance();
@@ -16,7 +23,7 @@ public class ZombieGame {
 	private Hero hero;
 	private int stage;
 
-	private String loads = "";
+	private boolean isRun = true;
 
 	private ZombieGame() {
 	}
@@ -28,72 +35,166 @@ public class ZombieGame {
 	}
 
 	public void run() {
-		hero=new Hero();
+		hero = new Hero();
 		units.add(hero);
-		while (true) {
+		while (isRun) {
 			if (units.size() == 1)
 				setStage();
+			printMap();
 			int sel = inputNum("[1]이동하기 [0]종료");
 
 			if (sel == MOVE)
-				move();
+				play();
 			else if (sel == EXIT) {
 				System.out.println("게임 종료.");
 				break;
 			}
 		}
-
-//		setGame();
-//		
-//		String props= "@#%&$";
-//		
-//		int pos = 0;
-//		for(int i=0;i<100;i++) {
-//			loads+=props.charAt(ran.nextInt(props.length())) ;
-//		}
-//		String def="▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒";
-//		while(pos+20 < loads.length()) {
-//			int defMax=def.length();
-//			int defPos=pos % defMax;
-//			String map="";
-//			if(defPos+20>defMax) {
-//				map=def.substring(defPos, defMax);
-//				map+=def.substring(0, defPos+20-defMax);
-//			}else
-//				map=def.substring(defPos, defPos+20);
-//			System.out.println("====================");
-//			System.out.println(map);
-//			System.out.println("    H        Z    B");
-//			
-//			System.out.println(map);
-//			System.out.println(loads.substring(pos, pos+20));
-//			System.out.println("--------------------");
-//			String input= scan.nextLine();
-//			System.out.println(pos);
-//			pos++;
-//		}
 	}
 
 	private void setStage() {
 		stage++;
+		hero.reset();
 		int pos = 0;
 		for (int i = 0; i < stage; i++) {
 			pos += ran.get(5, 10);
-			Zombie zombie = new Zombie(100 + stage * 10, 10 + stage, pos);
+			Zombie zombie = new Zombie(50 + stage * 5, 10 + stage, pos);
 			units.add(zombie);
 		}
-		pos += ran.get(5, 10);
-		Boss boss = new Boss(200 + stage * 50, 20 + stage, pos);
+		pos += ran.get(10, 15);
+		Boss boss = new Boss(100 + stage * 10, 15 + stage * 2, pos);
 		units.add(boss);
+		upgrade();
+	}
+
+	private void upgrade() {
+		int point = stage;
+		while (point > 0) {
+			System.out.println("UPGRADE >>>>");
+			hero.showStatus();
+			System.out.println("남은 포인트 : " + point);
+			int sel = inputNum("업그레이드 선택");
+
+			int hp = 0;
+			int pow = 0;
+			int acc = 0;
+			if (sel == HP) {
+				hp = 10;
+				point--;
+			} else if (sel == POW) {
+				pow = 2;
+				point--;
+			} else if (sel == ACC && hero.getAccuracy() < 90) {
+				acc = 1;
+				point--;
+			}
+
+			hero.upgrade(hp, pow, acc);
+		}
+		hero.showStatus();
+	}
+
+	private void printMap() {
+		String map = "\nSTAGE - " + stage;
+		map += "\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n";
+		int heroPos = hero.getPos();
+		int size = units.size();
+		ArrayList<String> unitsStr = new ArrayList<>();
+
+		for (int i = 0; i < MAP_SIZE; i++)
+			unitsStr.add(" ");
+		for (int i = 0; i < size; i++) {
+			Unit unit = units.get(i);
+			int pos = unit.getPos();
+			pos -= heroPos;
+			pos += 2;
+			if (pos >= 0 && pos < MAP_SIZE) {
+				unitsStr.set(pos, unit.getSymbol());
+			}
+		}
+		for (int i = 0; i < MAP_SIZE; i++)
+			map += unitsStr.get(i);
+
+		map += "\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n";
+		map += "Status : " + hero;
+		System.out.println(map);
+	}
+
+	private void play() {
+		move();
+		action();
 	}
 
 	private void move() {
-		
-		for(int i = 0; i < units.size(); i++) {
-			Unit unit=units.get(i);
-			unit.move();
-
+		int start = 0;
+		int end = 0;
+		int size = units.size();
+		for (int i = 0; i < units.size(); i++) {
+			Unit unit = units.get(i);
+			if (i > 0)
+				start = units.get(i - 1).getPos() + 1;
+			if (i < size - 1)
+				end = units.get(i + 1).getPos() - 1;
+			unit.move(start, end);
 		}
+	}
+
+	private void action() {
+		int heroPos = hero.getPos();
+		Unit enemy = units.get(1);
+
+		if (enemy.getPos() != heroPos + 1)
+			return;
+
+		printMap();
+		while (true) {
+			printBattle(enemy);
+			int sel = inputNum("[1]공격하기 [2]물약먹기");
+
+			if (sel == ATTACK) {
+				int ranOrder = ran.get(2);
+				if (ranOrder == 0) {// 공격순서 랜덤
+					if (attack(hero, enemy) || attack(enemy, hero))
+						return;
+				} else {
+					if (attack(enemy, hero) || attack(hero, enemy))
+						return;
+				}
+
+			} else if (sel == POTION) {
+				if (hero.getPotion() == 0) {
+					System.out.println("물약이 없습니다.");
+					continue;
+				}
+				hero.recovery();
+				if (attack(enemy, hero))
+					return;
+			}
+		}
+	}
+
+	private void printBattle(Unit enemy) {
+		String msg = String.format("\nBATTLE - %s VS %s\n", hero.getName(), enemy.getName());
+		msg += "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n";
+		msg += String.format(" %-17s%17s \n", hero, enemy);
+		msg += "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒";
+		System.out.println(msg);
+	}
+
+	private boolean attack(Unit attacker, Unit target) {
+		boolean isDead = attacker.attack(target);
+		if (isDead) {
+			if (target instanceof Hero)
+				isRun = false;
+			else {
+				units.remove(target);
+				int item = ran.get(2);
+				if (item == 0)
+					hero.addPotion();
+			}
+		}
+
+		return isDead;
 	}
 
 	private int inputNum(String msg) {
@@ -102,6 +203,7 @@ public class ZombieGame {
 
 		try {
 			int num = Integer.parseInt(input);
+			return num;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
